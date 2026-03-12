5d6be4f8f93389814c48eb2eb5268c6df21591af
Joemanji <107489778+joematopia@users.noreply.github.com>
2026-03-12 00:20:31 -0400

    build: add main.jsx React mount point
    Wires the React DOM to index.html and mounts the CitizenDashboard component for global broadcast.

---
+++ frontend/src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CitizenDashboard from './components/CitizenDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CitizenDashboard />
+  </React.StrictMode>,
+)
